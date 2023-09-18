81b21a102911075b083610c3e2ba1fe54c2f90d4
Da-achraf <achraf.boukir01@gmail.com>
2023-09-18 03:27:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      Pink: "hsl(322, 100%, 66%)",
+      LightPink: "hsl(321, 100%, 78%)",
+      LightRed: "hsl(0, 100%, 63%)",
+      VeryDarkCyan: "hsl(192, 100%, 9%)",
+      VeryPaleBlue: "hsl(207, 100%, 98%)",
+      Blue: "#1877F2",
+      InstaColor1: "#f9ce34",
+      InstaColor2: "#ee2a7b",
+      InstaColor3: "#6228d7",
+      White: "white",
+      LightGray: "#E6E7E7",
+      Transparent: "transparent",
+    },
+  },
+  plugins: [],
+};
